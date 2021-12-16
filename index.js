515bceb33b398744d74afb2f9ecdecb3601bae33
peter.hau <peterweb2005@gmail.com>
2021-12-16 13:14:28 +0800

    env

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
+
+// env
+require('dotenv').config();
+
+
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 const path = require('path');
@@ -22,6 +27,7 @@ const config = {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
 };
+console.log('config: ', config);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
